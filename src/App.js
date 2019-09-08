9cd1704341feb5c93943fe3a7fa17c8c82a74631
bharathijayanthi <bharathij@ezeees.com>
2019-09-08 11:35:49 +0530

    added signin page with forminput and custom button

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import { Route, Link, Switch} from 'react-router-dom';
 import Homepage from './pages/homepage/homepage.component';
 import Shop from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import Header from './components/header/header.component';
 const Hats = () => (
   <div>
     <h1>Hats</h1>
@@ -12,9 +14,11 @@ const Hats = () => (
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
       <Route exact path="/" component={Homepage} />
       <Route exact path="/shop" component={Shop} />
+      <Route exact path="/signin" component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import SignIn from '../../components/sign-in/sign-in.component';
+import './sign-in-and-sign-up.styles.scss';
+
+const SingnInAndSignUpPage = () => (
+    <div><SignIn /></div>
+)
+export default SingnInAndSignUpPage;
